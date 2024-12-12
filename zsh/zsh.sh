cb89a1172030d3d287ceb5f3721471e575ec3c65
root <root@vultr.guest>
2024-12-12 00:37:57 +0000

    docs: Add zsh terminal beautify plan

---
+++ zsh/zsh.sh
@@ -0,0 +1,22 @@
+echo $0
+
+cat /etc/shells
+
+sudo apt update
+sudo apt install zsh
+
+chsh -s $(which zsh)
+
+chsh -s /bin/bash
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# .zshrc
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+source ~/.zshrc
+
+p10k configure
+
