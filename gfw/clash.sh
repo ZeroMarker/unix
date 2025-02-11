301b61b02a4066d05cdbadb4be7fb58f3df3bac9
Mark Chen <mark.chen.im@gmail.com>
2025-02-11 16:06:40 +0800

    docs: gfw

---
+++ gfw/clash.sh
@@ -0,0 +1,10 @@
+git clone https://gh-proxy.com/https://github.com/nelvko/clash-for-linux-install.git \
+  && cd clash-for-linux-install \
+  && sudo bash -c '. install.sh; exec bash'
+
+clashon
+
+clashoff
+
+clashui
+
