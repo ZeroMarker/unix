703a45fe98aff69191be4faf2d31fa9b05a89437
Mark Chen <mark.chen.im@gmail.com>
2025-09-08 11:49:38 +0800

    fish variable

--- shell/fish/fish.sh
+++ shell/fish/fish.sh
@@ -1,5 +1,9 @@
 fish_add_path (pwd)
 
+vi ~/.config/fish/config.fish
+set -x my_var '213'
+source ~/.config/fish/config.fish
+
 sudo apt update
 
 sudo apt install fish
@@ -7,4 +11,5 @@ sudo apt install fish
 fish --version
 
 sudo chsh -s /usr/bin/fish
+
 
