d331df8d8875f4e0571945691cd569265818cf5c
Mark Chen <mark.chen.im@gmail.com>
2026-03-27 11:16:32 +0800

    ncdu

--- disk/ncdu.sh
+++ disk/ncdu.sh
@@ -2,4 +2,19 @@ sudo apt install ncdu
 
 ncdu
 
+ncdu --exclude ~/pik
+
+npm cache clean --force
+
+go clean -modcache -cache
+
+apt clean
+
+journalctl --disk-usage
+
+sudo journalctl --vacuum-time=7d
+
+cargo install cargo-cache
+cargo cache
+cargo cache --autoclean
 
