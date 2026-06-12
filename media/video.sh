7010ba75635f1337d3a1994a0ab50fadfea5dbc2
Mark Chen <mark.chen.im@gmail.com>
2026-06-12 09:37:36 +0800

    yazi

---
+++ media/video.sh
@@ -0,0 +1,14 @@
+asciiplayer
+
+termplay
+
+mpv
+
+后端	效果	命令示例
+tct	黑白字符画，兼容性最好	mpv --vo=tct 视频.mp4
+caca	彩色字符画，需要安装 libcaca	mpv --vo=caca 视频.mp4
+kitty	真彩色图片，仅限 Kitty 终端	mpv --vo=kitty 视频.mp4
+sixel	真彩色图片，需终端支持 Sixel	mpv --vo=sixel 视频.mp4
+null	无视频，仅音频	mpv --vo=null 视频.mp4
+
+
